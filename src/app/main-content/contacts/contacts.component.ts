9b414ef0d0fe5de599f252526b996f2b711422e8
Marco Elste <marco.elste@web.de>
2025-09-27 19:54:11 +0200

    chore: Added component ContactList, ContactGroup

--- src/app/main-content/contacts/contacts.component.ts
+++ src/app/main-content/contacts/contacts.component.ts
@@ -1,23 +1,14 @@
-import { Component, inject } from '@angular/core';
-import { AsyncPipe } from '@angular/common';
-import { DisplaySizeService } from '../../shared/services/display-size.service';
-import { FirebaseDBService } from '../../shared/services/firebase-db.service';
-import { ContactListComponent } from '../../shared/components/contact-list/contact-list.component';
-import { ContactDetailComponent } from '../../shared/components/contact-detail/contact-detail.component';
-import { DisplayType } from '../../shared/enums/display-type.enum';
+import { Component } from '@angular/core';
+import { ContactListComponent } from './contact-list/contact-list.component';
 
 @Component({
   selector: 'section[contacts]',
   imports: [
-    ContactListComponent,
-    ContactDetailComponent,
-    AsyncPipe
-],
+    ContactListComponent
+  ],
   templateUrl: './contacts.component.html',
   styleUrl: './contacts.component.scss'
 })
 export class ContactsComponent {
-  protected fireDB: FirebaseDBService = inject(FirebaseDBService);
-  protected dss: DisplaySizeService = inject(DisplaySizeService);
-  DisplayType = DisplayType;
+
 }
