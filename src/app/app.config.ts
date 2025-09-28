b193bcaed1a9896ec8c8329db595ab73f3051871
Rebell180 <buchmann_marcel@web.de>
2025-09-28 20:19:33 +0200

    implement fireContactService. icludes getAll, getOne, update, add and delete

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,6 @@ import { provideRouter } from '@angular/router';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { firebaseConfig } from './environment/environment';
-import { provideAnimations } from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
 
@@ -11,7 +10,6 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter( routes ), 
-    provideAnimations(),
     provideFirebaseApp(() => initializeApp( firebaseConfig )), 
     provideFirestore(() => getFirestore())]
 
