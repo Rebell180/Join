41cd08d144689a5685070767c1606f10b5f1d9f4
Marco Elste <marco.elste@web.de>
2025-09-28 22:18:55 +0200

    chore: Added Component SingleContact

--- src/app/main-content/contacts/contact-list/contact-group/single-contact/single-contact.component.ts
+++ src/app/main-content/contacts/contact-list/contact-group/single-contact/single-contact.component.ts
@@ -1,15 +1,12 @@
 import { Component, input } from '@angular/core';
 import { Contact } from '../../../../../shared/classes/contact';
-import { ContactIconComponent } from '../../../contact-icon/contact-icon.component';
 
 @Component({
   selector: 'app-single-contact',
-  imports: [
-    ContactIconComponent
-  ],
+  imports: [],
   templateUrl: './single-contact.component.html',
   styleUrl: './single-contact.component.scss'
 })
 export class SingleContactComponent {
-  contact = input.required<Contact>();  
+  contact = input.required<Contact>();
 }
