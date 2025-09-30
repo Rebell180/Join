e486d42686c725f6c1cc2de7137a3e7d3e5271d8
Prodigy2103 <marcusghne@gmx.de>
2025-09-30 12:48:03 +0200

    changed grid-template-rows in global content from auto 1fr in 10dvh and 90dvh

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { NavItemData } from './../interfaces/navitemdata';
 import { NavItemComponent } from "../nav-item/nav-item.component";
@@ -7,32 +8,11 @@ import { NavItemComponent } from "../nav-item/nav-item.component";
    * bedeutet das, die Komponente in ein normales Header-Tag eingebunden wird
    * Der Name der Komponente app-header muss sich dabei vom Tag <header> unterscheien. */
   selector: 'header[app-header]',
-  imports: [NavItemComponent], 
+  standalone: true,
+  imports: [CommonModule], 
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-  
-    item: Array<NavItemData> = [ 
-      {
-      sectionId: 'Summary',
-      title: 'Summary',
-      imagePath: 'assets/Icons/Summary.png'
-      },
-      {
-      sectionId: 'addTask',
-      title: 'Add task',
-      imagePath: 'assets/Icons/addTask.png'
-      },
-      {
-      sectionId: 'Boards', 
-      title: 'Boards',
-      imagePath: 'assets/Icons/Board.png'
-      },
-      {
-      sectionId: 'Contacts', 
-      title: 'Contacts',
-      imagePath: 'assets/Icons/Contacts.png'
-      }
-    ]
+
 }
