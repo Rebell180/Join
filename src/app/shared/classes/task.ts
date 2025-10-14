abea41156101f063105a406066b38a78972efca1
Prodigy2103 <marcusghne@gmx.de>
2025-10-14 14:56:52 +0200

    add task.ts and subTask.ts

--- src/app/shared/classes/task.ts
+++ src/app/shared/classes/task.ts
@@ -1,98 +1,58 @@
-import { Timestamp } from "@angular/fire/firestore";
-import { Category } from "../enums/category.enum";
-import { Priority } from "../enums/priority.enum";
-import { TaskStatusType } from "../enums/task-status-type";
-import { DBObject } from "../interfaces/db-object";
-import { Contact } from "./contact";
-import { SubTask } from "./subTask";
-import { TaskObject } from "../interfaces/task-object";
+export class Task {
 
-/**
- * Contains a single task object.
- */
-export class Task implements DBObject{
+	// #region Attributes
+	id: string;
+	title: string;
+	description: string;
+	dueDate: number;
+	priority: string;
+	category: string;
+	assignedTo: string;
+	subtasks: boolean = true;
+	// #endregion
 
-	// #region properties
-	
-	/** id of task in database, empty, if not exist in database */
-	id: string = '';
-
-	/** title of task, small and clear definition of task */
-	title: string = '';
-
-	/** description of task, extendet definition of task / user story */
-	description: string = '';
-
-	/** definition of date until task hast to be done, (not in past) */
-	dueDate: Timestamp = Timestamp.fromMillis(
-	  Timestamp.now().toMillis() + (24 * 60 * 60 * 1000)
-	);
-
-	/** priority of task, defines how important is task */
-	priority: Priority = Priority.MEDIUM;
-
-	/** category of task, defines which category this task belongs */
-	category: Category = Category.TASK;
-
-	/** ids of contacts, who assigned to this task */
-	assignedTo: string[] = [];
-
-	/** All contacts of  Task */
-	contacts: Contact[] = [];
-
-	/** an indicator wether this task has subtasks */
-	hasSubtasks: boolean = false;
-	
-	/** Includs all subtasks. */
-	subtasks: SubTask[] = [];
-
-	/** defines the current status of Task */
-	status: TaskStatusType = TaskStatusType.TODO;
-
-	// #endregion properties
-
-	/**
-     * @param data is optional and from @type object {
-     *      id: string          		=> id of task in database, empty, if not exist in database
-     *      title: string   			=> title of task, small and clear definition of task
-     *      decription: string    		=> description of task, extendet definition of task / user story
-     *      dueDate: string       		=> definition of date until task hast to be done, (not in past)
-     *      priority: Priority       	=> priority of task, defines how important is task
-     *      category: Category         	=> category of task, defines which category this task belongs
-     *      assignedTo: Array<Contact>  => an array of contact ids which are assigned to task 
-	 * 		subtasks: Array<SubTask>	=> an indicator wether this task has subtasks
-     * }
-     */
-	constructor(data?: TaskObject) {
-		if(data) {
-			this.id = data.id;
-			this.title = data.title;
-			this.description = data.description;
-			this.dueDate = data.dueDate;
-			this.priority = data.priority;
-			this.category = data.category;
-			this.assignedTo = data.assignedTo;
-			this.hasSubtasks = data.hasSubtasks;
-			this.status = data.status;
-		}
+	constructor({id = '', title, description, dueDate, priority, category, assignedTo, subtasks}:{
+		id: string,
+		title: string,
+		description: string,
+		dueDate: number,
+		priority: string,
+		category: string,
+		assignedTo: string,
+		subtasks: boolean,
+	}) {
+		this.id = id;
+		this.title = title;
+		this.description = description;
+		this.dueDate = dueDate;
+		this.priority = priority;
+		this.category = category;
+		this.assignedTo = assignedTo;
+		this.subtasks = subtasks;
 	}
 
 	/**
-     * Returns a JSON-string from tast.
-     * 
-     * @returns - the task data as JSON.
-    */
-	toJSON(): object {
+ * Returns a plain JSON-compatible object representing the current Task instance.
+ *
+ * @returns {Object} An object containing all Task properties.
+ *
+ * @description
+ * - convert the Task into a JSON string.
+ * - || operator provides default fallback values if a property is `undefined` or empty.
+ * - ?? mullish operator ensures that `false` for `subtasks`
+ *   is preserved and not overwritten by the default value.
+ */
+
+	toJsonTask() {
 		return {
-			id: this.id,
-			title: this.title,
-			description: this.description,
-			dueDate: this.dueDate,
-			priority: this.priority,
-			category: this.category,
-			assignedTo: this.assignedTo,
-			hasSubtasks: this.hasSubtasks,
-			status: this.status
+			id: this.id || "",
+			title: this.title || "",
+			description: this.description || "",
+			dueDate: this.dueDate || 0,
+			priority: this.priority || "",
+			category: this.category || "",
+			assignedTo: this.assignedTo || "",
+			subtasks: this.subtasks ?? true
 		}
 	}
 }
