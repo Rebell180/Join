0fcda72affe3c09959a0c3c539e1444ddc795639
Prodigy2103 <marcusghne@gmx.de>
2025-10-26 12:53:32 +0100

    adjusted img path and add (click)="modalService.openAddTaskModal()" import protected modalService: ModalService = inject(ModalService);

--- src/app/main-content/board/task-list-column/task-list-column.component.ts
+++ src/app/main-content/board/task-list-column/task-list-column.component.ts
@@ -1,7 +1,7 @@
 import { Component, inject, input, InputSignal } from '@angular/core';
 import { Task } from '../../../shared/classes/task';
 import { CommonModule } from '@angular/common';
-import { TaskColumnItemComponent } from './task-column-item/task-column-item.component';
+import { TaskColumnItemComponent } from '../../../shared/components/task-column-item/task-column-item.component';
 import { ModalService } from '../../../shared/services/modal.service';
 
 @Component({
@@ -14,8 +14,9 @@ import { ModalService } from '../../../shared/services/modal.service';
     styleUrl: './task-list-column.component.scss'
 })
 export class TaskListColumnComponent {
-  listName: InputSignal<string> = input.required<string>();
-  tasks: InputSignal<Task[]> = input.required<Task[]>()
 
-  protected modalService: ModalService = inject(ModalService);
+    listName: InputSignal<string> = input.required<string>();
+    tasks: InputSignal<Task[]> = input.required<Task[]>();
+
+    protected modalService: ModalService = inject(ModalService);
 }
